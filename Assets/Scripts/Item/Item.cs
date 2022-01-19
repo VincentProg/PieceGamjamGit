852a6cefa0180fbfecb1b67d2fe8aa374d5b3b95
Vincent <vincentboutin.dev@gmail.com>
2022-01-19 10:55:53 +0100

    Interaction system base

---
+++ Assets/Scripts/Item/Item.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        gameObject.AddComponent<ItemShader>();
+    }
+
+    public virtual void Interact()
+    {
+        print("Interact");
+    }
+
+    public virtual void StopInteraction()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Item/ItemInheritance/Bed.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bed : Item
+{
+
+    public PostProcessManager postProcessManager;
+
+    private void Start()
+    {
+        postProcessManager.bed = transform;
+    }
+
+    public override void Interact()
+    {
+        base.Interact();
+        postProcessManager.ActivateAnimationZoomInBed();
+    }
+
+}
