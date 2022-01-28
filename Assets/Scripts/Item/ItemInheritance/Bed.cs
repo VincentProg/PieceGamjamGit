01fb17ceee187992c7af9e1c64924938b5eb2328
Vincent <vincentboutin.dev@gmail.com>
2022-01-28 10:43:47 +0100

    integration

--- Assets/Scripts/Item/ItemInheritance/Bed.cs
+++ Assets/Scripts/Item/ItemInheritance/Bed.cs
@@ -19,12 +19,13 @@ public class Bed : Item
 
     public override void Interact(bool canBeDeactivated = true)
     {
-        print("canBeInteracted = " + canBeInteracted);
         if (!canBeInteracted) return;
-        print("canBeInteracted2 = " + canBeInteracted);
         base.Interact();
-        print("after base");
-        if (currentIndexDialog < dialogues.Length && isEnterComa) cDialogue.onDialogueEnd += player.EnterComa;
+        print(isEnterComa);
+        if (currentIndexDialog < dialogues.Length && isEnterComa)
+        {
+            cDialogue.onDialogueEnd += player.EnterComa;
+        }
         else if (isEnterComa)
         {
             player.EnterComa();
